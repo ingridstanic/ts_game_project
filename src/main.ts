aa54d901ba7be4f3d3f0cfbb204088ab25af4201
IngridStanic <ingridstanic@gmail.com>
2025-11-18 13:45:16 +0100

    implemented eventlistener searchform

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-import './style.css'
+import { createHTML } from "./htmlUtils";
+import { getGame } from "./services/gameService";
+import "./style.css";
+
+const searchGameForm = document.getElementById("searchGameForm");
+
+searchGameForm?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const gameInput = document.getElementById("searchInput") as HTMLInputElement;
+
+  if (gameInput) {
+    const search = gameInput.value;
+
+    if (search.length !== 0) {
+      const games = await getGame();
+      createHTML(games);
+    } else {
+      console.log("Ange en sökning...");
+    }
+  }
+});
